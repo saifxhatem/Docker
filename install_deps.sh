c0b1cf12e058fa3be934842803d85c2863882677
Saif Hatem <s.hatem@yodawy.com>
2022-11-04 20:44:30 +0200

    aaaaaaaaaaaaaaaaaaaa

--- install_deps.sh
+++ install_deps.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 echo "Bash script received input: $1"
-if [[ $1 == "fpm-alpine" ]]; then
+if [[ $1 =~ "alpine" ]]; then
+    echo "Alpine imagine"
     apk add --no-cache unzip shadow nodejs npm nano && curl -sS https://getcomposer.org/installer -o /tmp/composer-setup.php && php /tmp/composer-setup.php --install-dir=/usr/local/bin --filename=composer
 else
+    echo "Debian image"
     apt update && apt install -y unzip nodejs npm nano && curl -sS https://getcomposer.org/installer -o /tmp/composer-setup.php && php /tmp/composer-setup.php --install-dir=/usr/local/bin --filename=composer
 fi
