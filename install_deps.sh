163d5b48b7824a7e8fbfc8542fef5070e88d8999
Saif Hatem <s.hatem@yodawy.com>
2022-11-04 16:43:57 +0200

    Try to fetch dependencies from bash script

---
+++ install_deps.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+if [[ $1 =~ "alpine" ]]; then
+    apk add --no-cache unzip shadow nodejs npm nano && curl -sS https://getcomposer.org/installer -o /tmp/composer-setup.php && php /tmp/composer-setup.php --install-dir=/usr/local/bin --filename=composer
+else
+    apt update && apt install -y unzip nodejs npm nano && curl -sS https://getcomposer.org/installer -o /tmp/composer-setup.php && php /tmp/composer-setup.php --install-dir=/usr/local/bin --filename=composer
+fi
